4757605a2ea076b9b7a4acbd21c459a764976da8
Honchar Denys <crackeraki@gmail.com>
2017-11-29 22:54:01 +0200

    added css

--- wmodal.js
+++ wmodal.js
@@ -3,31 +3,28 @@ angular.module("wmodal_spinner.html", []).run(["$templateCache", function($templ
 	$templateCache.put("wmodal_spinner.html", "<div></div>");
 }]);
 angular.module("wmodal_modal.html", []).run(["$templateCache", function($templateCache) {
-	$templateCache.put("wmodal_modal.html", "<div class='modal'><button>Close</button><ng-transclude>This is the default content of the modal</ng-transclude></div>");
+	$templateCache.put("wmodal_modal.html", "<div class='modal'><div class='modal_fade' ng-click='close(); $root.bodyScrollHidden=false'></div><div class='modal_content viewer'><i class='icon icon-close close-m' ng-click='close(); $root.bodyScrollHidden=false'></i><ng-transclude>This is the default content of the modal</ng-transclude></div></div>");
 }]);
 angular.module("wmodal_service", [])
 .service('wmodal', function($compile, $rootScope){
 	"ngInject";
 	this.modals = [];
-	this.spinners = [];
 	this.modal = function(obj){
-		/*
-		*	templateUrl
-		*	controller
-		*/
 		if(!obj.id) obj.id = Date.now();
-		let modal = '<wmodal id="'+obj.id+'" data="'+obj.data+'">';
+		let modal = '<wmodal id="'+obj.id+'">';
 		if(obj.template) modal += obj.template;
 		else if(obj.templateUrl){
 			modal += '<ng-include src="';
 			modal += "'"+obj.templateUrl+"'";
-			modal += '"></ng-include>';
+			modal += '" ng-controller="wparent"></ng-include>';
 		}
 		modal += '</wmodal>';
 		this.modals.push(obj);
 		let body = angular.element(document).find('body').eq(0);
 		body.append($compile(angular.element(modal))($rootScope));
+		angular.element(document).find('html').addClass('bodynoscroll');
 	}
+	this.spinners = [];
 	this.spinner = function(obj){
 		if(!obj.id) obj.id = Date.now();
 		var spinner = '<wspinner id="'+obj.id+'">';
@@ -35,17 +32,53 @@ angular.module("wmodal_service", [])
 		this.spinners.push(obj);
 		document.body.innerHTML += spinner;
 	}
-}).directive('wmodal', function() {
+}).directive('wmodal', function(wmodal) {
 	"ngInject";
 	return {
 		restrict: 'E',
 		transclude: true,
 		scope: {
-			id: '@',
-			data: '='
-		},
-		templateUrl: 'wmodal_modal.html'
+			id: '@'
+		}, link: function(scope, el){
+			scope.close = function(){
+				for (var i = 0; i < wmodal.modals.length; i++) {
+					if(wmodal.modals[i].id==scope.id){
+						wmodal.modals.splice(i, 1);
+						break;
+					}
+				}
+				if(wmodal.modals.length == 0){
+					angular.element(document).find('html').removeClass('bodynoscroll');
+				}
+				if(scope.cb) scope.cb();
+				el.remove();
+			}
+			for (var i = 0; i < wmodal.modals.length; i++) {
+				if(wmodal.modals[i].id==scope.id){
+					wmodal.modals[i].close = scope.close;
+					scope._data = wmodal.modals[i];
+					for(var key in wmodal.modals[i]){
+						scope[key] = wmodal.modals[i][key];
+					}
+					break;
+				}
+			}
+		}, templateUrl: 'wmodal_modal.html'
 	};
+}).controller('wparent', function($scope, $timeout) {
+	"ngInject";
+	$timeout(function(){
+		if($scope.$parent.$parent._data){
+			for (var key in $scope.$parent.$parent._data) {
+				$scope[key] = $scope.$parent.$parent._data[key];
+			}
+		}
+		if($scope.$parent._data){
+			for (var key in $scope.$parent._data) {
+				$scope[key] = $scope.$parent._data[key];
+			}
+		}
+	});
 }).directive('wspinner', function() {
 	return {
 		restrict: 'E',
